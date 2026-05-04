9bb2a9142e3dca9dff285a1c24e5f724ec8dd1ff
vineela2025 <vineelakommojudevops@gmail.com>
2026-05-04 23:41:58 +0530

    updated files

--- docker/ec2files/docker.sh
+++ docker/ec2files/docker.sh
@@ -28,3 +28,17 @@ mv /tmp/eksctl /usr/local/bin
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+
+
+#kubectl installation commands
+chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+kubectl version
+
+#eksctl installation
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl  /usr/local/bin
+eksctl version
